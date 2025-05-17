ff74cf23f7bd1ac453f7c9f6c9d0719376da6c4d
Kritikaj2005 <kriti202jain@gmail.com>
2025-05-17 18:39:48 +0530

    Ignore node_modules folder

--- dc2/frontend/src/components/Navbar.jsx
+++ dc2/frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar({ scrollToInput, scrollToHome }) {
         {/* App Icon and Name */}
         <div className="flex items-center space-x-3">
           {/* App Icon (SVG) */}
-          <svg
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             width="30"
             height="30"
@@ -22,12 +22,33 @@ function Navbar({ scrollToInput, scrollToHome }) {
             <circle cx="12" cy="12" r="10" />
             <line x1="12" y1="8" x2="12" y2="16" />
             <line x1="8" y1="12" x2="16" y2="12" />
-          </svg>
-          <span className="text-lg font-semibold">BSS</span>
+          </svg> 
+         <span className="text-lg font-semibold">BSS</span>*/}
         </div>
 
         {/* Navigation Links with SVG icons */}
         <div className="flex space-x-4 text-sm">
+          <a
+    href="#"
+    onClick={scrollToHome}
+    className="flex items-center space-x-1 hover:underline hover:text-green-300"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="text-white"
+    >
+      <path d="M3 12l9-9 9 9v9a3 3 0 01-3 3h-12a3 3 0 01-3-3v-9z" />
+      <path d="M9 21v-6h6v6" />
+    </svg>
+    <span>Home</span>
+  </a>
           {/* Upload SVG Icon */}
           <a
             href="#"
